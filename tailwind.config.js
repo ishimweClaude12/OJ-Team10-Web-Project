f96dec233cecbc8686c0a94fa680782f15c1e8aa
Kingcedru <byishimocedrick@gmail.com>
2023-08-17 16:54:53 +0200

    title

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#8ACC50",
+        secondary: "#116355",
+        dark: "#454545",
+        darkest: "#000000",
+        base: "#fff",
+      },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
         para: ["Montserrat"],
